6f03c1aefffa3f05d650b37935dfc393fca6b90f
Pavel Zhurman <lp@grodno.ru>
2021-01-16 12:00:52 +0300

    4_2 task module v1.0 complete

---
+++ app4_2task/src/main/java/by/it/academy/app4_2task/MainActivity.kt
@@ -0,0 +1,35 @@
+package by.it.academy.app4_2task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity(), CircleCustomView.OnCustomClickListener {
+
+    private lateinit var switch: SwitchCompat
+    private lateinit var customView: CircleCustomView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app4_2task)
+        customView = findViewById(R.id.customView)
+
+        switch = findViewById(R.id.switchCompat)
+
+        customView.onCustomClickListener(this)
+
+    }
+
+    override fun onCustomClick(x: Int, y: Int, color: Int) {
+        if (!switch.isChecked) {
+            Snackbar.make(customView, "x= $x, y= $y", Snackbar.LENGTH_SHORT).setTextColor(color).show()
+        } else {
+            Toast.makeText(application, "x= $x, y= $y", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
+
